c7f8602c7c54901b31a1280092fe73a92fe4cad3
DodgiePapa <48364135+DodgiePapa@users.noreply.github.com>
2022-10-13 15:55:13 +0200

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,2 +1,2 @@
-auth_key='your-api-key'
+auth_key='e565b1b86d3547999e1b73cfe1c92e54'
 # get one from https://www.assemblyai.com/?utm_source=youtube&utm_medium=referral&utm_campaign=channel_assemblyai
